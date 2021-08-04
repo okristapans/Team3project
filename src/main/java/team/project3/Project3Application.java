ebc3b00846c9d6d4104a9f3c98e807ce5dde1281
okristapans <oskars.krsitapans@gmail.com>
2021-08-04 14:44:23 +0300

    Hello world

--- src/main/java/team/project3/Project3Application.java
+++ src/main/java/team/project3/Project3Application.java
@@ -3,13 +3,18 @@ package team.project3;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
-
+@RestController
 public class Project3Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Project3Application.class, args);
     }
-
+    @GetMapping
+    public String hello(){
+        return "Sveik pasaul!";
+    }
 }
